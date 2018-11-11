76abe3934fb72ea2297ce930789d3de52d4a741e
Prem Chotepanit <pchotepanit1@sheffield.ac.uk>
2018-11-11 17:13:57 +0000

    tfidf

--- Document_Retrieval_Assignment_Files/my_retriever.py
+++ Document_Retrieval_Assignment_Files/my_retriever.py
@@ -11,21 +11,16 @@ class Retrieve:
             self.Log = log10 #Save log10 function
             D = len(self.vectormodel) # |D|
             idf = {} #initial idf
-            tfidf = {}
+            tfidf = {} #initial tfidf
             for d,terms in index.items(): #Calculate for idf
-                df = len(terms)
-                idf[d] = self.Log(D/df)
+                df = len(terms) #All document containing term 'w'
+                idf[d] = self.Log(D/df) #obtaining idf(w)
             for tf,terms in self.vectormodel.items(): #Calculate for tfidf
-                tfidf[tf] = {}
-                print(tf)
-                for word,IDF in idf.items():
-                    if word in self.vectormodel[tf]:
-                        tfidf[tf][word] =self.vectormodel[tf][word]*idf[word]
-                    else:
-                        continue
-                    #print(idf[word])
-                    #tfidf[tf][word] =self.vectormodel[tf][word]*idf[word]
-            print("tfidf : ",tfidf)
+                tfidf[tf] = {} #initial tfidf vector
+                for word,TF in self.vectormodel[tf].items(): #Obtain term frequency of a vector
+                    tfidf[tf][word] = TF*idf[word] #Adding tfidf of each term in a document using tf(w,d)*idf(w)
+            self.idf = idf
+            self.tfidf = tfidf
         
     # Method performing retrieval for specified query
     def forQuery(self, query): #Query function
@@ -44,25 +39,48 @@ class Retrieve:
             sorted_similarity = sorted(similarity.keys(), key=lambda k: similarity[k], reverse=True) #Sorting values and get only first 10 values
             return sorted_similarity
         elif self.termWeighting ==  "tf": #Mode tf
-            #print("Mode : tf")
             size_q = sum([q**2 for q in list(query.values())])**(0.5) #Getting the size of query
             for d,terms in self.vectormodel.items():
-                qd = {k: terms[k]*query[k] for k in query if k in terms} 
-                if qd == {}: #If there is no query's term is the document, continue the loop
-                    continue
-                size_d = sum([t**2 for t in list(terms.values())])**(0.5) # Getting the size of d
-                size_qd = sum([QD**2 for QD in list(qd.values())])**(0.5) 
-                #print(size_qd)
-                #print(d," : ",qd)
-                #size_qd = len(qd)**(0.5) #Getting the size of qd
+                qd = {}
+                for k in query:
+                    if k in terms:
+                        qd[k] = terms[k]*query[k] #Calculate qd
+                    else:
+                        continue #If there is no query's term is the document, continue the loop
+                #size_d = sum([t**2 for t in list(terms.values())])**(0.5) # Getting the size of d
+                size_d = sum(x*x for x in terms.values())**(0.5) # Getting the size of d
+                #size_qd = sum([QD**2 for QD in list(qd.values())])**(0.5) # Getting the size of qd
+                size_qd = sum(x*x for x in qd.values())**(0.5)
                 similarity[d] = size_qd/(size_q*size_d) #Calculate for similarity using similar equation
             sorted_similarity = sorted(similarity.keys(), key=lambda k: similarity[k], reverse=True) #Sorting values and get only first 10 values
-            #print(similarity[sorted_similarity[0]])
-            #print(similarity[sorted_similarity[1]])
-            return sorted_similarity
-        elif self.termWeighting == "tfidf":
-            print("Mode : tfidf")
-        return list([1,2])
+            return sorted_similarity[0:10]
+        elif self.termWeighting == "tfidf": #Mode tfidf
+            ###calculate tfidf of query##
+            tfidf_query = {}
+            for tf,terms in query.items(): #Calculate for tfidf of query
+                tfidf_query[tf] = {}
+                for word,TF in query.items():
+                    if word in self.idf:
+                        tfidf_query[tf] = TF*self.idf[word]
+            #############################
+            size_q = sum([q**2 for q in list(tfidf_query.values())])**(0.5) #Getting the size of tfidf of query
+            for d,terms_tfidf in self.tfidf.items():
+                qd={}
+                for k in tfidf_query:
+                    if k in terms_tfidf:
+                        qd[k] = terms_tfidf[k]*tfidf_query[k] #calculate each qd
+                    else:
+                        continue
+                if qd == {}:
+                    continue #ignore null case
+                size_d = sum(x*x for x in terms_tfidf.values())**(0.5) # Getting the size of d
+                size_qd = sum(x*x for x in qd.values())**(0.5) # Getting the size of qd
+                similarity[d] = size_qd/(size_q*size_d) #Calculate for similarity using similar equation
+            sorted_similarity = sorted(similarity.keys(), key=lambda k: similarity[k], reverse=True) #Sorting values and get only first 10 values
+            print(sorted_similarity[0], " : ", similarity[1410])
+            return sorted_similarity[0:10]
+                
+        #return list([1,2])
         ###
     def Make_Vector_Model(self, Document):
         vector = {} #return the vector values as a dictionary object 
